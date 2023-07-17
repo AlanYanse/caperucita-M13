6d8bbda02c2ed03d12bfee23d3934b5b6a92425e
AlanYanse <yansealan@gmail.com>
2023-07-17 02:12:10 -0300

    configurando phaser

---
+++ src/script.js
@@ -0,0 +1,27 @@
+
+var config = {
+    type: Phaser.Auto,
+    width: 800,
+    height: 600,
+    scene: {
+
+        preload: preload,
+        create: create,
+        update: update
+
+    }
+};
+
+var game = new Phaser(config);
+
+function preload(){
+
+}
+
+function create(){
+
+}
+
+function update(){
+    
+}
